8728a4e42b8f5bc45f1530a52124c0cb3bae0b3f
vikohanyan <vikohanyan777@gmail.com>
2018-11-13 22:56:56 +0400

    add user migration

---
+++ console/migrations/m181113_185443_product.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m181113_185443_product
+ */
+class m181113_185443_product extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m181113_185443_product cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m181113_185443_product cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
